9ec24ca4ff2d13f48879db97377bf6bbc2e31c8d
cMcVay <connor.mcvay@yahoo.com>
2021-11-15 15:48:05 -0500

    make 5 and 12

--- mainScript.js
+++ mainScript.js
@@ -1,4 +1,14 @@
+let yesTwelve = true;
+
 function sayTwelve() {
-    document.getElementById("Dinner").innerHTML = 12;
+    if (yesTwelve) {
+        document.getElementById("Dinner").innerHTML = `<p class="big">12</p>`;
+        yesTwelve = false;
+        document.getElementById("changer").innerHTML = "Make 5"
+    } else {
+        document.getElementById("Dinner").innerHTML = "<p>5</p>"
+        yesTwelve = true;
+        document.getElementById("changer").innerHTML = "Press For 12"
+    }
 }
 
